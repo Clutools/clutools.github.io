c210f0e0f4ddaf3561417744d7323d63b36f829e
pg255 <raikala255@gmail.com>
2025-01-17 16:30:17 +0200

    Slideshow maker alpha

--- CSM/js.js
+++ CSM/js.js
@@ -10,6 +10,8 @@ function start() {
 	}
 	changeImage();
 	setInterval(changeImage, delay * 1000);
+	openFullscreen();
+
 }
 function changeImage() {
 	if (currentImage >= images.length) {
@@ -17,4 +19,37 @@ function changeImage() {
 	}
 	document.getElementById("img").src = images[currentImage];
 	currentImage++;
+}
+
+document.addEventListener("keypress", function (event) {
+	console.log(event.keyCode);
+	if (event.keyCode == 113) {
+		closeFullscreen();
+		location.reload();
+	}
+});
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
 }
